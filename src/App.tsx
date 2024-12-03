19534dd592658010d7dee5ef73de224ede342d3c
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-12-03 23:56:43 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import CodeMirror, { ViewUpdate } from "@uiw/react-codemirror";
+import { useCallback, useState } from "react";
+
+function App() {
+  const [value, setValue] = useState("");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onChange = useCallback((val: string, _: ViewUpdate) => {
+    console.log("val:", val);
+    setValue(val);
+  }, []);
+  return (
+    <>
+      <CodeMirror value={value} height="200px" onChange={onChange} />
+    </>
+  );
+}
+
+export default App;
